4d71d7c39fea454ccfedf081e643899bc2785892
dilanramirz <100289179+dilanramirz@users.noreply.github.com>
2022-03-10 21:38:48 -0500

    Add files via upload

---
+++ actividad2.py
@@ -0,0 +1,28 @@
+"""
+        ++++++++++++++++++
+        |MANEJO DE LISTAS|
+        ++++++++++++++++++
+"""
+def lista():
+    lista1 =[]
+    
+
+    listaconelementos= [25, 2500000, "Dilan Ramirez", "Estudiante ing.sistemas", True, [" 9 Semestre", 2022]]
+    
+    print("Motrando elemetos con el ciclo for ")
+    for i in range(len(listaconelementos)): 
+        print(listaconelementos[i])
+    print("\n\n")
+    
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()

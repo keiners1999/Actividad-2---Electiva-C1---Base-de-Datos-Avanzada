28f8a2eb3d7cbd75691d212cb179fdee08bf3deb
dilanramirz <100289179+dilanramirz@users.noreply.github.com>
2022-03-10 21:51:24 -0500

    Add files via upload

--- actividad2.py
+++ actividad2.py
@@ -7,14 +7,49 @@ def lista():
     lista1 =[]
     
 
-    listaconelementos= [25, 2500000, "Dilan Ramirez", "Estudiante ing.sistemas", True, [" 9 Semestre", 2022]]
+    listaconelementos= [25, 2500000, "Dilan Ramirez", "Estudiante ing.sistemas", True, [" 9 Semestre", "pediodo 2022"]]
     
     print("Motrando elemetos con el ciclo for ")
     for i in range(len(listaconelementos)): 
         print(listaconelementos[i])
     print("\n\n")
     
+    print("Mostrando elementos con el ciclo while")
+    j=0
+    while j < (len(listaconelementos)):
+        print(listaconelementos[j])
+        j+=1  
 
+    print("\n")
+    print("Imprimir una lista que está dentro de otra")
+    print(listaconelementos[-1][1])
+
+    print("\n")
+    print("mostrando elementos con saltos ")
+    print("Elementos Pares\n")
+    print(listaconelementos[0:len(listaconelementos):2])
+
+    print("\nElementos Impares\n")
+    print(listaconelementos[1:len(listaconelementos):2])
+
+    print("\n")
+    print("añadir un elemento al final")
+    listaconelementos.append(["Sede maicao", "Mario Pelaez"])
+    print(listaconelementos)
+
+    print("\n")
+    print("añadir un elemento en una posicion")
+    listaconelementos.insert(2,"elemento añadido")
+    print(listaconelementos)
+
+    print("\n")
+    print("Eliminar un elemento")
+    listaconelementos.pop(2)
+    print(listaconelementos)
+
+    listaconelementos.remove("Estudiante ing.sistemas") 
+def main():
+    lista()
 
 if __name__ == "__main__":
     main()
